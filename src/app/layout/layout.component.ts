defe1deed5830568a6449043ff36e58522c03bae
Joel Estumano <joelestumano@gmail.com>
2023-01-12 15:31:50 -0300

    Remoção código inútil

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,19 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
-export class LayoutComponent {
-
-  public open: boolean = false;
-
- /*  constructor(private readonly menuResponsivoService: MenuDisplayService) {
-    this.menuResponsivoService.getSource().subscribe((isOpen) => {
-      if (isOpen) {
-        this.open = false;
-      } else {
-        this.open = true;
-      }
-    });
-  } */
-}
+export class LayoutComponent {}
