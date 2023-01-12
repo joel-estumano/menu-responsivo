d6cd395433075f266636b703bea4b2707f430d14
Joel Estumano <joelestumano@gmail.com>
2023-01-12 10:40:50 -0300

    Correção inicialização do menu

--- src/app/layout/components/menu-responsivo/menu-responsivo.component.ts
+++ src/app/layout/components/menu-responsivo/menu-responsivo.component.ts
@@ -1,8 +1,8 @@
 import {
+  AfterViewInit,
   Component,
   ElementRef,
   HostListener,
-  OnInit,
   Renderer2,
   ViewChild,
 } from '@angular/core';
@@ -13,7 +13,7 @@ import { MenuControleService } from './menu-controle.service';
   templateUrl: './menu-responsivo.component.html',
   styleUrls: ['./menu-responsivo.component.css'],
 })
-export class MenuResponsivoComponent implements OnInit {
+export class MenuResponsivoComponent implements AfterViewInit {
   @ViewChild('menu') menu: ElementRef = {} as ElementRef;
   private readonly menuClassico = 'd-flex app-menu-responsivo p-4';
   private readonly menuOffcanvas = 'd-flex offcanvas-collapse open p-4';
@@ -24,20 +24,21 @@ export class MenuResponsivoComponent implements OnInit {
   @HostListener('window:scroll', ['$event'])
   @HostListener('window:resize', ['$event'])
   onWindowScroll(event?: any) {
-     if (this.elementRef.nativeElement.offsetParent?.className) {
+    if (this.elementRef.nativeElement.offsetParent?.className) {
       console.log('não existia parent');
       this.mControlService.setVisivel(true);
     } else {
       console.log('existia parent');
       this.mControlService.setVisivel(false);
-    } 
+    }
   }
 
   constructor(
     private mControlService: MenuControleService,
     private elementRef: ElementRef,
     private renderer2: Renderer2
-  ) {
+  ) {}
+  ngAfterViewInit(): void {
     this.mControlService.getSourceMenuOpen().subscribe((isOpen) => {
       this.open = isOpen;
 
@@ -58,8 +59,6 @@ export class MenuResponsivoComponent implements OnInit {
       }
     });
   }
-
-  ngOnInit(): void {}
 }
 
 //  this.renderer2.setAttribute(this.menu.nativeElement, 'class', 'd-flex app-menu-responsivo p-4');
